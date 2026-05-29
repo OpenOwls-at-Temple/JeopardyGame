d8d59e722e3446b14f02ed840a5cbd298c9ece8b
Zirong Wong <156629524+wshiyou@users.noreply.github.com>
2026-05-29 13:55:39 +0900

    Revert "Phase1"

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-// https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [react()],
-    // relative base so the production build also works when opened from disk
-    base: './',
-});
